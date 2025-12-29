7cefc7e5f1f8bff2a7dd93d5c87b35097e5053a9
hyebin-dev <229252157+hyebin-dev@users.noreply.github.com>
2025-12-29 23:00:52 +0900

    chore: init backend Spring Boot project (JDK17, MySQL, ddl-auto=none)

---
+++ backend/src/test/java/com/hyebin/ott/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyebin.ott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
